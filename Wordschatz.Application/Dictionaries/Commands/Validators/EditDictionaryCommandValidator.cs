2c273a13ac6157f0f2663e5041338a877bec0474
Grumissimus <55281603+Grumissimus@users.noreply.github.com>
2020-05-31 01:38:28 +0200

    Adding missing messages to the EditDictionaryCommandValidator

--- Wordschatz.Application/Dictionaries/Commands/Validators/EditDictionaryCommandValidator.cs
+++ Wordschatz.Application/Dictionaries/Commands/Validators/EditDictionaryCommandValidator.cs
@@ -8,19 +8,18 @@ namespace Wordschatz.Application.Dictionaries.Commands.Validators
         public EditDictionaryCommandValidator()
         {
             RuleFor(command => command.DictionaryId)
-                .NotNull()
-                .GreaterThanOrEqualTo(1);
+                .GreaterThanOrEqualTo(1).WithMessage("The identifier of the dictionary must be a positive non-zero number.");
 
             RuleFor(command => command.Name)
-                .NotNull();
+                .NotNull().WithMessage("The name of the dictionary cannot be null.")
+                .NotEmpty().WithMessage("The dictionary must have a name.");
 
             RuleFor(command => command.Description)
-                .NotNull();
+                .NotNull().WithMessage("The description of the dictionary cannot be null.");
 
             RuleFor(command => command.Password)
-                .NotNull()
-                .NotEmpty()
-                .When(command => command.VisibilityLevel == Visibility.PasswordProtected);
+                .NotNull().WithMessage("The password cannot be null.").When(command => command.VisibilityLevel == Visibility.PasswordProtected)
+                .NotEmpty().WithMessage("The dictionary with password-protected visibility level must have a password.").When(command => command.VisibilityLevel == Visibility.PasswordProtected);
         }
     }
 }
